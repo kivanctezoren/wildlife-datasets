dd3ee067a2e33d515223a05780c256835d439f3d
Kıvanç Tezören <kivanc.tezoeren@uni-siegen.de>
2026-07-07 11:20:01 +0200

    Port custom splits

---
+++ wildlife_datasets/splits/minimal_gallery_split.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+
+from . import IdentitySplit
+
+
+class MinimalGallerySplit(IdentitySplit):
+    #returns only one database image per individual
+    def __init__(self, ratio_train : float = 1, **kwargs):
+        self.ratio_train = ratio_train
+        super().__init__(**kwargs)
+
+    def split(self, df : pd.DataFrame) -> list[tuple[np.ndarray, np.ndarray]]:
+        lcg = self.initialize_lcg()
+        y_counts = df[self.col_label].value_counts()
+        class_names = y_counts.index.to_numpy()
+        idx_test = lcg.random_permutation(df.shape[0])
+        
+        idx_train = []
+        classes_included = []
+        #draw random items until we have one from each class
+        for i in range(df.shape[0] - 1, -1, -1):
+            idx = idx_test[i]
+            current_class = df.at[idx, "identity"]
+            if current_class not in classes_included:
+                classes_included.append(current_class)
+                idx_train.append(int(idx))
+                idx_test = np.delete(idx_test, i)
+            if (len(class_names) * self.ratio_train) <= len(classes_included):
+                break
+        idx_train = np.array(idx_train)
+        
+        return [(idx_train, idx_test)]
--- wildlife_datasets/splits/time_aware_split.py
+++ wildlife_datasets/splits/time_aware_split.py
@@ -297,3 +297,148 @@ class RandomProportion:
 
     def set_col_label(self, *args, **kwargs):
         self.splitter.set_col_label(*args, **kwargs)
+
+
+class VideoDateProportionOpenSetSplit(TimeAwareSplit):
+    """Time-proportion open set splitting method into training and testing sets.
+
+    Mostly copied from TimeProportionOpenSetSplit from wildlife_datasets
+
+    First, it pust some individuals into the training set only.
+    Then it is the TimeProportionSplit.
+    """
+
+    def __init__(
+            self,
+            ratio_train: float,
+            ratio_class_test: float = None,
+            n_class_test: int = None,
+            **kwargs
+            ) -> None:
+        """Initializes the class.
+
+        The user must provide exactly one from `ratio_class_test` and `n_class_test`.
+        The latter specifies the number of individuals to be only in the testing set.
+        The former specified the ratio of samples of individuals (not individuals themselves)
+        to be only in the testing set.
+
+        Args:
+            ratio_train (float): *Approximate* size of the training set.
+            ratio_class_test (float, optional): *Approximate* ratio of samples of individuals only in the testing set.
+            n_class_test (int, optional): Number of individuals only in the testing set.
+            seed (int, optional): Initial seed for the LCG random generator.
+            identity_skip (str, optional): Name of the identities to ignore.
+            col_label (str, optional): Column name containing individual animal names (labels).
+        """
+
+        if ratio_class_test is None and n_class_test is None:
+            raise(Exception('Either ratio_class_test or n_class_test must be provided.'))
+        elif ratio_class_test is not None and n_class_test is not None:
+            raise(Exception('Only ratio_class_test or n_class_test can be provided.'))
+        
+        self.ratio_train = ratio_train
+        self.ratio_class_test = ratio_class_test
+        self.n_class_test = n_class_test
+        super().__init__(**kwargs)
+
+    def modify_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Prepares dataframe for splits.
+
+        Removes identities specified in `self.identity_skip` (usually unknown identities).
+        Convert the `date` column into a unified format.
+        Add the `year` column.
+
+        Args:
+            df (pd.DataFrame): A dataframe of the data. It must contain columns `identity` and `date`.
+
+        Returns:
+            Modified dataframe of the data.
+        """
+        
+        # Check if the DataFrame contain the column date.
+        if 'vid_date' not in df.columns:
+            raise(Exception('Dataframe df does not contain column vid_date.'))
+        
+        # Remove identities to be skipped
+        df = df.copy()
+        df = df[df[self.col_label] != self.identity_skip]
+
+        # Removes entries without dates
+        df = df[~df['vid_date'].isnull()]
+        
+        # Convert date to datetime format (from possibly strings) and drop hours
+        df['vid_date'] = pd.to_datetime(df['vid_date']).apply(lambda x: x.date())
+        df['year'] = df['vid_date'].apply(lambda x: x.year).to_numpy()            
+        return df
+
+    def split(self, df: pd.DataFrame) -> List[Tuple[np.ndarray, np.ndarray]]:
+        """Implementation of the [base splitting method](../reference_splits#splits.balanced_split.BalancedSplit.split).
+
+        Args:
+            df (pd.DataFrame): A dataframe of the data. It must contain columns `identity` and `date`.
+
+        Returns:
+            List of splits. Each split is list of labels of the training and testing sets.
+        """
+        
+        df = self.modify_df(df)
+
+        # Initialize the random number generator
+        lcg = self.initialize_lcg()
+
+        # Compute the counts and randomly permute them
+        y_counts = df[self.col_label].value_counts()
+        n_class = len(y_counts)
+        idx = lcg.random_permutation(n_class)
+        y_counts = y_counts.iloc[idx]
+
+        # Compute number of identities in the testing set
+        n = len(df)
+        if self.n_class_test is None:
+            n_test = np.round(n*self.ratio_class_test).astype(int)
+            n_class_test = np.where(np.cumsum(y_counts) >= n_test)[0][0] + 1
+        else:
+            n_class_test = self.n_class_test
+
+        # Specify individuals going purely into training and testing sets
+        individual_train = np.array([], dtype=object)
+        individual_test = np.array(y_counts.index[:n_class_test])
+
+        # Compute how many samples go automatically to the training and testing sets
+        y_counts = df[self.col_label].value_counts()
+        n_train = sum([y_counts.loc[y] for y in individual_train])
+        n_test = sum([y_counts.loc[y] for y in individual_test])
+        
+        # Recompute ratio_train and adjust it to proper bounds
+        ratio_train = self.ratio_train
+        if n_train + n_test > 0 and n_train + n_test < n:
+            ratio_train = (n*ratio_train - n_train) / (n - n_test - n_train)
+        ratio_train = np.clip(ratio_train, 0, 1)
+
+        idx_train = []
+        idx_test = []
+        # Loop over all identities; x is a tuple (identity, df with unique identity)
+        for name, df_name in df.groupby(self.col_label):
+            if name in individual_train and name in individual_test:
+                # Check if the class does not belong to both sets
+                raise(Exception('Individual cannot be both in individual_train and individual_test.'))
+            elif name in individual_train:
+                # Check if the class does not belong to the training set
+                idx_train += list(df_name.index)
+            elif name in individual_test:
+                # Check if the class does not belong to the testing set
+                idx_test += list(df_name.index)
+            else:
+                dates = df_name.groupby('vid_date')
+                n_dates = len(dates)
+                if n_dates > 1:
+                    # Loop over all dates; y is a tuple (date, df with unique date and identity)
+                    for i, (_, df_date) in enumerate(dates):
+                        # Add half dates to the training and half to the testing set
+                        if i < np.minimum(n_dates-1, int(np.round(ratio_train*n_dates))):
+                            idx_train += list(df_date.index)
+                        else:
+                            idx_test += list(df_date.index)
+                else:
+                    idx_train += list(df_name.index)
+        return [(np.array(idx_train), np.array(idx_test))]
